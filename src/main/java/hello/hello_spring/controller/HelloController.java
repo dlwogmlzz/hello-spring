d5083c4d3954cc0f1cd65e5ea8e0824a977d0e2a
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-13 11:30:34 +0900

    20260413_SpringStudy_RequestParam을 이용한 MVC템플릿엔진

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -5,6 +5,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "이재희님!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
