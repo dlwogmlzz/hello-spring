94a4cabf8a35656a4e2dee0b9cf2fbd1f7f685f9
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-06-02 11:11:10 +0900

    20260602_SpringStudy_회원도메인과 리포지토리 만들기..

--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package hello.hello_spring.repository;
 
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository {
+    Member save(Member member); // 회원저장용..
+
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); // 지금 까지의 회원리스트를 반환...
 }
